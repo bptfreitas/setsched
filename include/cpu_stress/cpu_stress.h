d36520fa5d8af79494b99447ae0604a924ae0348
Bruno <brunopolicarpo@hotmail.com>
2025-09-08 19:15:28 -0300

    Create 'cpu_stress' program to create CPU-Bound processes easily

---
+++ include/cpu_stress/cpu_stress.h
@@ -0,0 +1,7 @@
+#ifndef __CPU_STRESS_H__
+
+#define __CPU_STRESS_H__
+
+
+
+#endif
---
+++ src/cpu_stress/cpu_intensive.c
@@ -0,0 +1,12 @@
+#include <cpu_stress/cpu_stress.h>
+
+#include <stdio.h>
+#include <limits.h>
+
+void CPU_intensive(long int max){
+
+    for ( long int i = 0; i < max ; i ++ ){
+
+    }
+
+}
